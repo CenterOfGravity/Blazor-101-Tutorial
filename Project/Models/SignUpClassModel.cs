1945b5f98fa8d3e3cdc6ea8b4a67281d259c3271
jhanif.kara <jhanif.kara@outlook.com>
2022-12-28 10:57:20 -0500

    SignUpPage Done

---
+++ Project/Models/SignUpClassModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class SignUpClassModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
